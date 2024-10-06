0ed9f02d9382d0db498af01542b0d4c4c4743f53
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-10-06 19:31:58 +0600

    features: dinamically show all pets

--- scripts/app.js
+++ scripts/app.js
@@ -41,4 +41,77 @@ const displayCategories = (categories) =>{
         
     });
 }
+
+// load all pets
+const loadPets = async()=>{
+    const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
+    const data = await response.json();
+    displayPets(data.pets);
+}
+
+// display pet
+const displayPets = (pets) =>{
+    const petsContainer = document.getElementById('pets-container');
+    pets.forEach(pet => {
+        const div = document.createElement('div');
+        div.classList = "p-5 border border-gray-200 rounded-xl space-y-6";
+        const {breed,category,date_of_birth,price,image,gender,pet_name} = pet;
+        div.innerHTML = `
+                        <img
+                  class="w-full rounded-lg"
+                  src=${image}
+                  alt="${category} image"
+                />
+                <!-- card content -->
+                <div class="">
+                  <div class="border-b pb-3 mb-3 space-y-1">
+                    <h3 class="text-lg md:text-xl font-bold text-black">
+                      ${pet_name}
+                    </h3>
+                    <span class="flex gap-2 text-base text-secondary/70">
+                      <img src="./asstets/icons/breed.png" alt="breed icon" />
+                      <p>Breed: ${breed ? breed : "Unknown"}</p>
+                    </span>
+                    <span class="flex gap-2 text-base text-secondary/70">
+                      <img src="./asstets/icons/birth.png" alt="birth icon" />
+                      <p>Birth: ${date_of_birth ? date_of_birth : "Unknown"}</p>
+                    </span>
+                    <span class="flex gap-2 text-base text-secondary/70">
+                      <img src="./asstets/icons/gender.png" alt="breed icon" />
+                      <p>Gender: ${gender ? gender : "Unknown"}</p>
+                    </span>
+                    <span class="flex gap-2 text-base text-secondary/70">
+                      <img src="./asstets/icons/price.png" alt="breed icon" />
+                      <p>Price: ${price ? price + '$' : "Unknown"}</p>
+                    </span>
+                  </div>
+                  <!-- card button start -->
+                  <div class="flex justify-between">
+                    <button
+                      class="h-8 md:h-10 px-4 md:px-5 border-2 border-gray-200 hover:border-primary/20 hover:bg-primary/20 rounded-lg text-gray-500 font-bold text-base lg:text-lg"
+                    >
+                      <i class="fa-regular fa-thumbs-up"></i>
+                    </button>
+                    <button
+                      class="h-8 md:h-10 px-4 md:px-5 border-2 border-gray-200 hover:border-primary/20 hover:bg-primary/20 rounded-lg text-primary font-bold text-base lg:text-lg"
+                    >
+                      Adopt
+                    </button>
+                    <button
+                      
+                      class="h-8 md:h-10 px-4 md:px-5 border-2 border-gray-200 hover:border-primary/20 hover:bg-primary/20 rounded-lg text-primary font-bold text-base lg:text-lg"
+                    >
+                      Details
+                    </button>
+                  </div>
+                  <!-- card button end -->
+                </div>
+                <!-- card content end -->
+        `;
+
+        petsContainer.appendChild(div);
+    });
+}
+
 loadCategories();
+loadPets();
