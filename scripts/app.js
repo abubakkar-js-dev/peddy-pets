5aad9730a367c216c177727a16bbaabf845612f1
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-10-06 11:24:21 +0600

    completed designt adopts best friend section

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
 const menuBar = document.getElementById('menu-bar');
-const menuSection = document.getElementById('menu-sec')
+const menuSection = document.getElementById('menu-sec');
+const viewMoreBtn = document.getElementById('btn-view-more');
 menuBar.addEventListener('click',()=>{
     if(menuSection.classList.contains('hidden')){
         menuSection.classList.remove('hidden');
     }else{
         menuSection.classList.add('hidden');
     }
+});
+
+// scroll to adopt best friend sec when view more btn clicked 
+viewMoreBtn.addEventListener('click',()=>{
+    document.getElementById('adopt-best-friend').scrollIntoView({behavior: 'smooth'});
 })
