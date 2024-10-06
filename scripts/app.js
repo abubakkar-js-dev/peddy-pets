82a886c9eca42de825893626b47606899c58635d
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-10-06 06:03:29 +0600

    add: completed navbar design section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-console.log('Connected')
+const menuBar = document.getElementById('menu-bar');
+const menuSection = document.getElementById('menu-sec')
+menuBar.addEventListener('click',()=>{
+    if(menuSection.classList.contains('hidden')){
+        menuSection.classList.remove('hidden');
+    }else{
+        menuSection.classList.add('hidden');
+    }
+})
